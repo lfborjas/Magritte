36988e4c9f03823aa627609436fe5ae5073f724f
Luis Felipe Borjas <luis.borjas@escolarea.com>
2010-05-16 19:48:48 -0600

    listo el ponderador de categorías

--- lebrixen/search/management/commands/ponder_categories.py
+++ lebrixen/search/management/commands/ponder_categories.py
@@ -1,31 +1,43 @@
+#encoding=utf-8
 '''
 Created on 09/05/2010
-
-@author: lfborjas
+author: lfborjas
 '''
 from __future__ import division
 from django.conf import settings
-from search.models import DmozCategory, DocumentSurrogate
-from django.core.management.base import NoArgsCommand, CommandError
+from search.models import DmozCategory
+from django.core.management.base import NoArgsCommand
 import logging
-from django.db.models import Count
+#import guppy.heapy.RM
+#from django.db.models import Count
 
 class Command(NoArgsCommand):
-    help = "Update the category classifier"
+    help = "Ponder the edges in the ontology graph by an inclusion statistical approximation"
     
     def handle_noargs(self, **options):
         #get the maximum number of documents any category has
-        docs = DocumentSurrogate.objects.values('category').annotate(ccount=Count('category')).iterator()
-        #docs me da info de la cantidad de docs por categoría, así que puedo trabajar con esto... (y quizá un
-        #iterador en las categorías
-        maxcount = max(docs, key=lambda x: x['ccount'])['ccount']
+        #docs = DocumentSurrogate.objects.values('category').annotate(ccount=Count('category')).iterator()        
+        #maxcount = max(docs, key=lambda x: x['ccount'])['ccount']
         
         #set the utility of the categories relative to that size
         categories = DmozCategory.objects.iterator()
-        for category in categories: 
-            category_docs = category.documentsurrogate_set.count()
-            category.weight = 1 / (maxcount / category_docs) if category_docs else 1/maxcount
-            category.save()
+        c = 0
+        for category in categories:             
+            s = category.documentsurrogate_set.count()
+            sub_categories = category.dmozcategory_set.all() 
+            for sc in sub_categories:
+                #if it has no documents, count the category as a document
+                s += sc.documentsurrogate_set.count() or 1            
+            
+            for sc in sub_categories:
+                sub_docs = sc.documentsurrogate_set.count()
+                #give a minimum non-zero weight if this category has no documents:
+                sc.weight = sub_docs / s if sub_docs else 1 / s
+                sc.save()
+            c += 1
+        
+        logging.debug("Done pondering %s categories" % c)
+                
 
         
         
