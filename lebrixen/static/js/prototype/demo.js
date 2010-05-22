3d8695d8d35ab0314858b0f5d99cff8db8cad2d2
Luis Felipe Borjas <luis.borjas@escolarea.com>
2010-05-22 00:47:38 -0600

    Puestos keybindings para presentación de demo: izquierda grafo, arriba tools, abajo unranked, derecha ranked

--- lebrixen/static/js/prototype/demo.js
+++ lebrixen/static/js/prototype/demo.js
@@ -1,2 +1,28 @@
-var appId="";
-var appUser="";
+$(function(){
+	//bind some keys to the document ready:
+	//based on http://seriouscoding.com/keyboard-navigation-with-javascript-and-jquer
+	$(document).keydown(function(event){
+		var key = event.keyCode || event.which;
+		var element="";		
+		if(event.target.type !== 'text' && event.target.type !== 'select'){
+			//LEFT = graph
+			if(key=== 37){
+				element = '#user-profile img';
+			}
+			//UP=tools
+			else if(key===38){
+				element = '#tools';
+			}
+			//RIGHT=simulation unranked
+			else if(key===39){
+				element = "#simulation-unranked";
+			}
+			//DOWN = simulation ranked
+			else if(key===40){
+				element = "#simulation-ranked";
+			}
+			$(element).effect('highlight', {color: '#f6f6f6'}, 1000);
+			
+		}//if not a text or a select
+	});
+});
