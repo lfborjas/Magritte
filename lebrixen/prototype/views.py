76ecdb7b139daddd26f50de6d76c38df4b575617
Luis Felipe Borjas <luis.borjas@escolarea.com>
2010-05-21 18:05:11 -0600

    puestos parámetros de feedback y user id en el prototipo

--- lebrixen/prototype/views.py
+++ lebrixen/prototype/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 def index(request):
     """Index view for the prototype page"""
+    feedbackMode = request.GET.get('fm', 'follow')
+    userId = request.GET.get('uid', 'testUser')
     return render_to_response('prototype_base.html',
-                               {'form':PrototypePlanningForm(initial={'lang': 'en'})},
+                               {'form':PrototypePlanningForm(initial={'lang': 'en'}),
+                                'uid': userId,
+                                'fm': feedbackMode},
                                 context_instance=RequestContext(request))
