ef5626d6cc5c931eddbfcb70df2d7bcd01f2c486
Luis Felipe Borjas <luis.borjas@escolarea.com>
2010-05-23 17:49:09 -0600

    listo el form de registro

--- lebrixen/register/views.py
+++ lebrixen/register/views.py
@@ -4,14 +4,16 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
+
 def register(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
             url = form.cleaned_data['url']            
             recipient = form.cleaned_data['mail']
-            key = form.cleaned_data['app'].get_token()
-            message = render_to_string('mail/appkey.html', {'key': key,
+            key = form.cleaned_data['key']
+            message = render_to_string('mail/appkey.html', {'app': url,
+                                                            'key': key,
                                                             'url': request.build_absolute_uri('/usage/')})
             send_mail(
                 'Your T-Recs App key',
