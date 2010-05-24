3ecac4835aab08fb66986c8535517f5042a85bc4
Luis Felipe Borjas <luis.borjas@escolarea.com>
2010-05-24 11:06:49 -0600

    Corregido error en get_for_query: dividía el weight entre 100, cuando el peso YA ES un valor [0,1]

--- lebrixen/search/models.py
+++ lebrixen/search/models.py
@@ -58,7 +58,7 @@ class DmozCategory(models.Model):
             #rval += [{'category_id': result.data['category_id'],
             #          'category_title': result.data['category_title'],
             #          'relevance': result.rank},]
-            rval.update({int(result.data['category_id'][0]): result.weight/100})
+            rval.update({int(result.data['category_id'][0]): result.weight})
         
         s_conn.close()
         if not rval:
