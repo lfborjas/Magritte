d84591abf5516687e94566a0538d5ee7d5c391c8
Luis Felipe Borjas <luis.borjas@escolarea.com>
2010-06-23 18:39:09 -0600

    creado otro caso de prueba, parchado el selenium test runner para que no cree tantas db en vano (aún no sé por qué diablos lo hace); a ver cómo se comporta esto con las fixtures...

--- lebrixen/register/selenium/test_register.py
+++ lebrixen/register/selenium/test_register.py
@@ -7,6 +7,8 @@ from selenium import selenium
 import time, re
 from django.test import TestCase
 from django.core import mail
+import logging
+#import unittest
 
 class test_register(TestCase):
     def setUp(self):
@@ -23,12 +25,28 @@ class test_register(TestCase):
         sel.click("register-submit")
         sel.click("register-submit")
         sel.wait_for_page_to_load("30000")
+        #test redirection:
         try: self.failUnless('/usage/' in sel.get_location())
         except AssertionError, e: self.verificationErrors.append(str(e))
+        #test the email:
         try: self.assertEquals(len(mail.outbox),1)
         except AssertionError, e: self.verificationErrors.append(str(e))
         try: self.assert_('Your T-Recs App key' in mail.outbox[0].subject or 'Su clave' in mail.outbox[0].subject)
-        except AssertionError, e: self.verificationErrors.append(str(e))        
+        except AssertionError, e: self.verificationErrors.append(str(e))
+    
+    def test_empty_url(self):
+        """Test that attempts to register with a blank username fail"""
+        sel = self.selenium
+        sel.open("/register/")                      
+        sel.type("id_mail", "new@app.com")
+        sel.click("register-submit")        
+        sel.wait_for_page_to_load("30000")
+        #test error reporting:
+        txt=sel.get_text("//form[@id='register-form']/table/tbody/tr[1]/td[2]/ul/li")
+        try: self.assert_("This field is required" in txt or "Este campo es obligatorio" in txt 
+                           , 'Should have reported it')
+        except AssertionError, e: self.verificationErrors.append(str(e))
+                
         
     def tearDown(self):
         self.selenium.stop()
