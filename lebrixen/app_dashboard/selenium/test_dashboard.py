f8d03d38421b41045f69a7bbece6d41d2ae1f6cc
Luis Felipe Borjas <luis.borjas@escolarea.com>
2010-06-24 18:02:54 -0600

    selenium no puede con los tabs :(

--- lebrixen/app_dashboard/selenium/test_dashboard.py
+++ lebrixen/app_dashboard/selenium/test_dashboard.py
@@ -15,11 +15,12 @@ class TestDashboard(TransactionTestCase):
         self.verificationErrors = []
         self.selenium = selenium("localhost", 4444, "*chrome", "http://localhost:8000/")
         self.selenium.start()
-        self.selenium.set_speed(2000)
+        #self.selenium.set_speed(2000)
         self.selenium.window_maximize()
         self.app = ClientApp.objects.get_or_create(url='fixtureapp.tmp')[0]
         self.pwd = "test"
-        self.user=None
+        #self.user=None
+        
         try:
             self.app_user =  User.objects.create_user(self.app.url, 'testapp@localhost.com', self.pwd)
             self.app.user = self.app_user
@@ -27,14 +28,9 @@ class TestDashboard(TransactionTestCase):
         except:
             logging.debug("error saving user", exc_info=True)
             pass
-    
-    def test_register_users(self):
+        
+        #login for every test:
         sel = self.selenium
-        #always login:
-#        sel.open("/dashboard/logout/")
-#        sel.wait_for_page_to_load("30000")
-#        sel.open('/dashboard/')
-#        logging.debug("opened dashboard...")        
         sel.open("/login/?next=/")
         sel.wait_for_page_to_load("30000")                
         sel.type("id_username", self.app.user.username)
@@ -44,6 +40,14 @@ class TestDashboard(TransactionTestCase):
         
         try: self.assert_('/login/' not in sel.get_location(), 'Should have logged in')
         except AssertionError, e: self.verificationErrors.append(str(e))
+    
+    def test_register_users(self):
+        sel = self.selenium
+        #always login:
+#        sel.open("/dashboard/logout/")
+#        sel.wait_for_page_to_load("30000")
+#        sel.open('/dashboard/')
+#        logging.debug("opened dashboard...")             
         
         limit = settings.FREE_USER_LIMIT - self.app.users.count() 
         if not limit or limit <=2:
@@ -74,14 +78,38 @@ class TestDashboard(TransactionTestCase):
         except AssertionError, e: self.verificationErrors.append(str(e))
         try: self.failUnless(sel.is_text_present(u2))
         except AssertionError, e: self.verificationErrors.append(str(e))
-
+    
+    def test_users_list(self):
+        """Test that an app's users are correctly listed"""
+        self.selenium.set_speed(2000)              
+        sel = self.selenium
+        users = []
+        for u in range(settings.FREE_USER_LIMIT/2):
+            users.append(self.app.users.create(clientId="testUser_%s"%uuid4()))
+        
+        sel.open("/")        
+        sel.wait_for_page_to_load("30000")
+        sel.wait_for_condition('selenium.browserbot.getCurrentWindow().jQuery("#tabs").tabs("select",1);\
+        selenium.browserbot.getCurrentWindow().jQuery("#tabs").tabs("load",1);',
+                                 3000)
+        for user in users:
+            try: self.failUnless(sel.is_text_present(user.clientId))
+            except AssertionError, e: self.verificationErrors.append(str(e))
+            try: self.failUnless(sel.is_text_present(str(user.added.date())))
+            except AssertionError, e: self.verificationErrors.append(str(e))
+            
+         
+            
+        
+        
+    
     
     def tearDown(self):
-        try:
-            self.app.users.all().delete()
-        except: 
-            logging.debug("no users to delete")
-            pass
+#        try:
+#            self.app.users.all().delete()
+#        except: 
+#            logging.debug("no users to delete")
+#            pass
         self.selenium.stop()
         self.assertEqual([], self.verificationErrors)
 
