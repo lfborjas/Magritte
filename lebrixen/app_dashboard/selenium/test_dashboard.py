ad88f2a38e6c1536c377dd93be08194f43738467
Luis Felipe Borjas <luis.borjas@escolarea.com>
2010-06-25 02:44:49 -0600

    listas las pruebas de lo del dashboard

--- lebrixen/app_dashboard/selenium/test_dashboard.py
+++ lebrixen/app_dashboard/selenium/test_dashboard.py
@@ -1,3 +1,4 @@
+#encoding=utf-8
 from selenium import selenium
 import time, re
 from django.test import TransactionTestCase
@@ -42,6 +43,7 @@ class TestDashboard(TransactionTestCase):
         except AssertionError, e: self.verificationErrors.append(str(e))
     
     def test_register_users(self):
+        """Test regular user registration"""
         sel = self.selenium
         #always login:
 #        sel.open("/dashboard/logout/")
@@ -80,29 +82,78 @@ class TestDashboard(TransactionTestCase):
         except AssertionError, e: self.verificationErrors.append(str(e))
     
     def test_users_list(self):
-        """Test that an app's users are correctly listed"""
-        self.selenium.set_speed(2000)              
+        """Test that an app's users are correctly listed"""           
         sel = self.selenium
         users = []
-        for u in range(settings.FREE_USER_LIMIT/2):
+        for u in range(settings.FREE_USER_LIMIT/3):
             users.append(self.app.users.create(clientId="testUser_%s"%uuid4()))
         
-        sel.open("/")        
+        sel.open("/users/")        
         sel.wait_for_page_to_load("30000")
-        sel.wait_for_condition('selenium.browserbot.getCurrentWindow().jQuery("#tabs").tabs("select",1);\
-        selenium.browserbot.getCurrentWindow().jQuery("#tabs").tabs("load",1);',
-                                 3000)
+#        sel.wait_for_condition('selenium.browserbot.getCurrentWindow().jQuery("#tabs").tabs("select",1);\
+#        selenium.browserbot.getCurrentWindow().jQuery("#tabs").tabs("load",1);',
+#                                 3000)
         for user in users:
             try: self.failUnless(sel.is_text_present(user.clientId))
             except AssertionError, e: self.verificationErrors.append(str(e))
             try: self.failUnless(sel.is_text_present(str(user.added.date())))
             except AssertionError, e: self.verificationErrors.append(str(e))
-            
-         
-            
-        
-        
     
+    def test_limit_respect(self):
+        """Test that we can only add users up to the limit"""
+        sel = self.selenium
+
+        users = []        
+        for u in range(settings.FREE_USER_LIMIT):
+            users.append("testUser_%s"%uuid4())
+        
+        
+        sel.type("id_form-0-name", users[0])
+        #register users till it tops:
+        for i,u in enumerate(users[1:]):
+            sel.wait_for_condition('selenium.browserbot.getCurrentWindow().jQuery("#register-users-form a.add-row").trigger("click")',
+                                    3000)
+        #sel.click("css=#register-users-form a.add-row")                                
+            sel.type("form-%s-id_form-0-name"%(i+1), u)
+            
+        cnd = sel.wait_for_condition('!Boolean(selenium.browserbot.getCurrentWindow().jQuery("a.add-row").is(":visible"));',
+                                                  20000)
+                
+        
+        #register
+        sel.click("css=input.trecs-submit")                
+        sel.wait_for_page_to_load("30000")
+            
+        #the warning should be here, and the submit button, not:        
+        try: self.assert_(sel.is_element_present('css=span.warning'), 'Warning should be here')
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        
+        txt=sel.get_text("css=span.warning")
+        
+        try: self.assert_(u"Límite de usuarios excedido" in txt 
+                          or "User limit exceeded" in txt, 'Warning should be here')
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        
+        
+        try: self.assertFalse(sel.is_element_present('css=input.trecs-submit'), 'REGISTER Should NOT be present!')
+        except AssertionError, e: self.verificationErrors.append(str(e))
+        
+        #the users should be listed:
+        sel.open("/users/")        
+        sel.wait_for_page_to_load("30000")
+        for user in users:
+            try: self.failUnless(sel.is_text_present(user))
+            except AssertionError, e: self.verificationErrors.append(str(e))            
+        
+#    def test_delete(self):
+#        """Test that an user can be deleted"""
+#        #has the impasse of the jquery tab...
+#        pass
+#        
+#    def test_delete_update(self):
+#        #has the impasse of the jquery tab...
+#        """Test that, when an app is full, deleting n random users results in being able to add again"""
+#        pass
     
     def tearDown(self):
 #        try:
