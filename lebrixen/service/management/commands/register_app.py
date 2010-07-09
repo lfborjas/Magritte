0a2778f26d478642d1d2706f99bdb38ea457ec03
Luis Felipe Borjas <luis.borjas@escolarea.com>
2010-07-09 15:04:12 -0600

    modificado el comando de registro de apps desde línea de comandos para crear el user también

--- lebrixen/service/management/commands/register_app.py
+++ lebrixen/service/management/commands/register_app.py
@@ -9,20 +9,31 @@ from django.conf import settings
 from profile.models import ClientApp
 from django.core.management.base import BaseCommand, CommandError
 from optparse import make_option
+from django.contrib.auth.models import User 
 
 class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
                   make_option('--url', dest='app_url',                    
                     help='The url of the app to register'),
+                  make_option('--email', dest='app_email',                    
+                    help='The contact email of the app to register'),
                                              )
-    help = "The url of the app to register"
-    args = 'url'      
+    help = "Register an app and get it's credentials"
+    args = 'url email'      
                 
-    def handle(self, url, *args, **options):
+    def handle(self, url, email, *args, **options):
         try:
             app = ClientApp(url=url)
             app.save()
-            return app.get_token()
+            
+            raw_pass = User.objects.make_random_password()
+            u = User.objects.create_user(app.url, email, raw_pass)
+            u.save()                                           
+            #logging.debug('pass for %s: %s' % (app.url, raw_pass))
+            app.user = u
+            app.save()
+            
+            return app.get_token(), app.user.username, raw_pass
         except Exception, e:
             raise CommandError(e.message)
 
